00bbae22b72d343655d1c6dd48507f66b67ea6f2
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-02-01 00:31:31 -0500

    Switch factory_bot to factory_bot_rails. Work on user specs. Comment out not used specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'unit testing' do
+    let(:built) { build(:user) }
+    
+    context 'general' do
+      it 'should be able to save successfully with valid input' do
+        saved = built.save
+        expect(saved).to eq true
+      end
+    end
+
+    context 'email' do
+      it 'should validate' do
+        user = build(:user, email: nil)
+        saved = user.save
+        expect(saved).to eq false
+      end
+
+      it 'should not allow duplicates, case insensitive' do
+        built.save
+        user = build(:user, password: 'PasSworD')
+        saved = user.save
+        expect(saved).to eq false
+      end
+
+      it 'should now allow more than 254 chars' do
+        user = build(:user, email: 'a' * 255)
+        saved = user.save
+        expect(saved).to eq false
+      end
+
+      it 'should verify email regex' do
+        bad_emails = []
+        bad_emails << build(:user, email: 'test').save
+        bad_emails << build(:user, email: 'test@m').save
+        bad_emails << build(:user, email: 'test@m.').save
+        bad_emails << build(:user, email: '@m.').save
+        bad_emails << build(:user, email: '@m.c').save
+        expect(bad_emails.any?).to eq false
+      end
+    end
+
+    context 'password' do
+      it 'should validate' do
+        user = build(:user, password: nil)
+        saved = user.save
+        expect(saved).to eq false
+      end
+
+      it 'should enforce length < 6' do
+        user = build(:user, password: 'fail')
+        saved = user.save
+        expect(saved).to eq false
+      end
+    end
+  end
 end
