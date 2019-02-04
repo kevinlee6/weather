92b29bec3bf388e7ffd70da154977c4187bfb268
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-02-04 18:28:30 -0500

    Add styling to weather info box.

--- frontend/src/components/WeatherContainer/WeatherInfo.js
+++ frontend/src/components/WeatherContainer/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { UNITS } from 'constant';
+import { titleCase } from 'helpers';
 import styled from 'styled-components';
 
 const Div = styled.div`
@@ -10,6 +11,38 @@ const Div = styled.div`
   padding: 30px;
   margin: 30px;
   font-weight: 600;
+  font-size: 1.3em;
+  display: flex;
+
+  @media (max-width: 992px) {
+    width: 80%;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const Location = styled.p`
+  font-size: 2em;
+`;
+
+const Temperature = styled.div``;
+
+const LeftColumn = styled.div`
+  flex-grow: 4;
+`;
+
+const RightColumn = styled.div`
+  flex-grow: 1;
+`;
+
+const CurrentTemp = styled.p`
+  font-size: 1.6em;
+`;
+
+const Status = styled.p`
+  font-size: 1.4em;
 `;
 
 const WIND = 'WIND';
@@ -18,6 +51,7 @@ const TEMP = 'TEMP';
 class WeatherInfo extends Component {
   render() {
     const { weather, unit } = this.props;
+    const status = titleCase(weather.weather);
     const { temp, location, humidity, windSpeed } = weather;
     const { min, max, cur } = temp;
     const { city, country } = location;
@@ -26,17 +60,21 @@ class WeatherInfo extends Component {
     const tempUnit = units[TEMP];
     return (
       <Div>
-        <div>
-          <p>{`${cur}${tempUnit}`}</p>
-          <div>
-            <p>Low: {`${min}${tempUnit}`}</p>
-            <p>High: {`${max}${tempUnit}`}</p>
-          </div>
-        </div>
-        <p>Location: {`${city}, ${country}`}</p>
-
-        <p>Humidity: {humidity}%</p>
-        <p>Windspeed: {`${windSpeed}${windUnit}`}</p>
+        <LeftColumn>
+          <Location>{`${city}, ${country}`}</Location>
+          <p>Humidity: {humidity}%</p>
+          <p>Windspeed: {`${windSpeed}${windUnit}`}</p>
+        </LeftColumn>
+        <RightColumn>
+          <Status>{status}</Status>
+          <Temperature>
+            <CurrentTemp>{`${cur}${tempUnit}`}</CurrentTemp>
+            <div>
+              <p>Low: {`${min}${tempUnit}`}</p>
+              <p>High: {`${max}${tempUnit}`}</p>
+            </div>
+          </Temperature>
+        </RightColumn>
       </Div>
     );
   }
--- frontend/src/components/WeatherContainer/index.js
+++ frontend/src/components/WeatherContainer/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import WeatherForm from './WeatherForm';
 import WeatherInfo from './WeatherInfo';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default () => (
-  <div>
+  <Div>
     <WeatherForm />
     <WeatherInfo />
-  </div>
+  </Div>
 );
