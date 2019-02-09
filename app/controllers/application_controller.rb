fd1a2cf4c6ea02c3d55ecd8d15e6366d75364041
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-02-08 21:01:11 -0500

    Test if rails catch all is causing production sign out error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   include ::ActionController::Cookies
 
-  def react_app
-    render file: 'public/index.html'
-  end
+  # def react_app
+  #   render file: 'public/index.html'
+  # end
 end
