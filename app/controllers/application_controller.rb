e265389157bf3df00453a9e8552374e91ace6f46
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-02-09 01:55:15 -0500

    Try including responds_to to test BrowserHistory blank page bug in production.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
   include ::ActionController::Cookies
 
-  def react_app
-    render file: 'frontend/public/index.html'
+  def fallback_index_html
+    respond_to do |format|
+      format.html { render body: Rails.root.join('public/index.html').read }
+    end
   end
 end
