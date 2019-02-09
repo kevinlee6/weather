c9abe753bfa121ba19daeec759b360d2db7eb5b4
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-02-09 15:04:01 -0500

    Connect sidebar visible status to redux store.

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -3,5 +3,6 @@ import auth from './auth';
 import weather from './weather';
 import unit from './unit';
 import favorite from './favorite';
+import sidebar from './sidebar';
 
-export default combineReducers({ auth, weather, unit, favorite });
+export default combineReducers({ auth, weather, unit, favorite, sidebar });
