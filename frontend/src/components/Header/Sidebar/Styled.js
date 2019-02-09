0236e3d889ce1b6fc2c04c74a820e0c88ffdf94d
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-02-09 15:04:49 -0500

    Create associated sidebar redux actions/reducers.

--- frontend/src/components/Header/Sidebar/Styled.js
+++ frontend/src/components/Header/Sidebar/Styled.js
@@ -1,17 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { List } from "antd";
-import styled from "styled-components";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { List } from 'antd';
+import { hideSidebar } from 'actions';
+import styled from 'styled-components';
 
 const StyledLink = styled(Link)`
   width: 80%;
   font-size: 1.6em;
 `;
 
-export const ListItem = ({ children, link = "/", handleClick }) => (
+export const ListItem = connect(
+  null,
+  { hideSidebar }
+)(({ children, link = '/', handleClick, hideSidebar }) => (
   <List.Item>
-    <StyledLink to={link} onClick={handleClick}>
+    <StyledLink
+      to={link}
+      onClick={() => hideSidebar() && handleClick && handleClick()}
+    >
       {children}
     </StyledLink>
   </List.Item>
-);
+));
--- frontend/src/components/Header/Sidebar/index.js
+++ frontend/src/components/Header/Sidebar/index.js
@@ -1,23 +1,13 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { reorderFavorite } from "actions";
-import { Drawer, Icon as AntdIcon } from "antd";
-import AuthLinks from "./AuthLinks";
-import FavoriteList from "./FavoriteList";
-import { DragDropContext } from "react-beautiful-dnd";
-import styled from "styled-components";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { reorderFavorite, showSidebar, hideSidebar } from 'actions';
+import { Drawer, Icon as AntdIcon } from 'antd';
+import AuthLinks from './AuthLinks';
+import FavoriteList from './FavoriteList';
+import { DragDropContext } from 'react-beautiful-dnd';
+import styled from 'styled-components';
 
 class Sidebar extends Component {
-  state = { visible: false };
-
-  showDrawer = () => {
-    this.setState({ visible: true });
-  };
-
-  onClose = () => {
-    this.setState({ visible: false });
-  };
-
   onDragEnd = result => {
     const { reorderFavorite } = this.props;
     const { destination, source } = result;
@@ -31,25 +21,25 @@ class Sidebar extends Component {
     // priority starts at 1; not zero-index based
     reorderFavorite({
       destination: destination.index + 1,
-      source: source.index + 1
+      source: source.index + 1,
     });
   };
 
   render() {
-    const { auth } = this.props;
+    const { auth, sidebar, showSidebar, hideSidebar } = this.props;
     const { authenticated } = auth;
     return (
       <div>
         <Icon
           className="sidebar-icon"
           type="menu-unfold"
-          onClick={this.showDrawer}
+          onClick={showSidebar}
         />
         <Drawer
-          title={<AuthLinks closeDrawer={this.onClose} />}
+          title={<AuthLinks />}
           placement="left"
-          onClose={this.onClose}
-          visible={this.state.visible}
+          onClose={hideSidebar}
+          visible={sidebar}
         >
           {authenticated ? (
             <DragDropContext onDragEnd={this.onDragEnd}>
@@ -68,13 +58,13 @@ class Sidebar extends Component {
 }
 
 const mapStateToProps = state => {
-  const { auth } = state;
-  return { auth };
+  const { auth, sidebar } = state;
+  return { auth, sidebar };
 };
 
 export default connect(
   mapStateToProps,
-  { reorderFavorite }
+  { reorderFavorite, showSidebar, hideSidebar }
 )(Sidebar);
 
 const Div = styled.div`
