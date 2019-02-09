efecffa7f68bb87b90f658ea131cac5790ac5865
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-02-08 19:57:29 -0500

    Minor changes in syntax to get routing to work with heroku.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   include ::ActionController::Cookies
 
-  def fallback_index_html
+  def react_app
     render file: 'public/index.html'
   end
 end
