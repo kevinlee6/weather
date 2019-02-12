f47b1fc888cbdc6fc438e856e11b1e8c6f553b98
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-02-12 13:40:46 -0500

    Refactor save location logic to helper module.

--- app/controllers/user_locations_controller.rb
+++ app/controllers/user_locations_controller.rb
@@ -1,5 +1,6 @@
 class UserLocationsController < ApplicationController
   include AuthHelper
+  include UserLocationsHelper
   before_action :get_user, only: [:index, :create, :reorder]
   before_action :get_location, only: [:create]
   before_action :get_user_locations, only: [:index, :reorder]
@@ -33,43 +34,7 @@ class UserLocationsController < ApplicationController
 
   def reorder
     size = @user_locations.size
-    to_splice = update_params[:source]
-    to_insert = update_params[:destination]
-    return if
-      to_splice == to_insert || 
-      to_splice < 0 ||
-      to_insert < 0 ||
-      to_splice > size ||
-      to_insert > size
-
-    query =
-      <<-SQL
-        UPDATE user_locations
-        SET priority = CASE
-          WHEN priority = #{to_splice} THEN #{to_insert}
-
-          WHEN #{to_insert} > #{to_splice}
-            AND priority <= #{to_insert}
-            AND priority > #{to_splice}
-            THEN priority - 1
-
-          WHEN #{to_insert} < #{to_splice}
-            AND priority >= #{to_insert}
-            AND priority < #{to_splice}
-            THEN priority + 1
-
-          ELSE priority
-        END
-        WHERE user_locations.user_id = #{@user.id}
-      SQL
-
-    res = ActiveRecord::Base.connection.execute(query)
-    if res
-      # back to subtract one for favorite reducer to handle in frontend
-      render json: { destination: to_insert, source: to_splice}
-    else
-      render json: { error: 'Database error for patch request' }
-    end
+    handle_reorder(size, update_params)
   end
 
   def destroy
