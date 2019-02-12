b284c52de540d00f3210d403fb3226624c61d8f9
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-02-12 12:26:09 -0500

    Change algorithm when deleting a saved location and assigning new priority.

--- app/models/user_location.rb
+++ app/models/user_location.rb
@@ -4,7 +4,6 @@ class UserLocation < ApplicationRecord
   belongs_to :user
   belongs_to :location
 
-  # priority 0 will be used as temp value when swapping priorities
   validates :priority, presence: true,
                        numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates_uniqueness_of :location_id, scope: :user_id
@@ -15,6 +14,6 @@ class UserLocation < ApplicationRecord
     user = User.find user_id
     return { error: 'No user found' } if !user
     user_locations = user.user_locations
-    self.priority = user_locations.length + 1
+    self.priority = user_locations.length
   end
 end
